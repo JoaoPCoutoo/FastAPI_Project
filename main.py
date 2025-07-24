14dfc3ca78a482a78f06e30784b6861200570bd5
JoaoPCoutoo <jcouto581@gmail.com>
2025-07-24 13:33:53 -0300

    Adicionando JWT para limitar acesso a rotas de acesso restrito

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
 import os 
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 
 #Para rodar o projeto, no terminal, basta rodar o código uvicorn main:app --reload
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
- 
+oauth2_schema= OAuth2PasswordBearer(tokenUrl="auth/login-form")
+
 #importando s rotas necessárias após instanciar app. Importante: o nome precisa ser o mesmo definido pelo arquivo referenciado
 
 from auth_routes import auth_routers
